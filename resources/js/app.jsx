7d7bf98ed83c188931cb6ca64cc795fb43a5f0a3
gelbiasa <gelbifirmansyah12@gmail.com>
2025-07-06 23:36:31 +0700

    push

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,14 @@
 import './bootstrap';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Dashboard from './Pages/Dashboard';
+import App from './Components/App';
 
 const container = document.getElementById('app');
 if (container) {
     const root = createRoot(container);
     root.render(
         <React.StrictMode>
-            <Dashboard/>
+            <App />
         </React.StrictMode>
     );
 } else {
