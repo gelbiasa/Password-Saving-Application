1920e96e30ba7c8da54579dee588d4f6d7b3b29f
gelbiasa <gelbifirmansyah12@gmail.com>
2025-08-03 21:54:44 +0700

    file balde detail password

---
+++ resources/views/pages/ManagementPassword/DetailPassword/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Detail Password - Pengelola Password</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100">
+    <div id="react-app"></div>
+    
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
